ec18efafd5572ff41e9a906872f80bd3e24f6589
Will Breaden Madden <wbm@protonmail.ch>
2018-03-08 19:14:18 +0000

    add lock.py, add setup.py, add documentation

---
+++ setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import os
+
+import setuptools
+
+def main():
+
+    setuptools.setup(
+        name             = "lock",
+        version          = "2018.03.08.1911",
+        description      = "module for enabling file locks",
+        long_description = long_description(),
+        url              = "https://github.com/wdbm/lock",
+        author           = "Will Breaden Madden",
+        author_email     = "wbm@protonmail.ch",
+        license          = "GPLv3",
+        py_modules       = [
+                           "lock"
+                           ],
+        entry_points     = """
+                           [console_scripts]
+                           lock = lock:lock
+                           """
+    )
+
+def long_description(
+    filename = "README.md"
+    ):
+
+    if os.path.isfile(os.path.expandvars(filename)):
+        try:
+            import pypandoc
+            long_description = pypandoc.convert_file(filename, "rst")
+        except ImportError:
+            long_description = open(filename).read()
+    else:
+        long_description = ""
+    return long_description
+
+if __name__ == "__main__":
+    main()
